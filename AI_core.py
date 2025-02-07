97d09bf2425d64e43a9e2d56b4a86786c40862ac
Crepsydus <crepsydyesno@gmail.com>
2025-02-07 18:15:32 +1000

    4.4 - corner priority

--- AI_core.py
+++ AI_core.py
@@ -72,9 +72,19 @@ def get_most_known():
             if adj in opened:
                 if adj not in flags:
                     knowledge += 2
+                    if map[adj] == 1:
+                        knowledge += 1
                 else:
                     knowledge += 1
-        knowledge += 2 * count_open_corner(coords)
+        knowledge += 3 * count_open_corner(coords)
+        for tile in get_adjacent(coords):
+            if tile in opened and tile not in flags:
+                local_km = 0
+                for i in get_adjacent(tile):
+                    if i in flags:
+                        local_km += 1
+                if local_km == map[tile]:
+                    knowledge += 2
         knowledge_list = np.concatenate((knowledge_list, np.array([[f"{coords[0]},{coords[1]}", str(knowledge)]])), axis=0)
     # return tuple([int(j) for j in knowledge_list[knowledge_list[:,1].tolist().index(str(max([int(i) for i in knowledge_list[:,1]]))), 0].split(",")])
     return knowledge_list
@@ -193,7 +203,7 @@ model = Sequential([
     Dense(64, activation='relu'),                      # Ещё один скрытый слой
     Dense(2, activation='softmax')                     # Выходной слой для 3 классов
 ])
-#     |||    4.3    |||
+#     |||    4.4    |||
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss='categorical_crossentropy',
@@ -206,10 +216,9 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
 # 4.1 - Taught to count to 8, added anti-bomb-overflowing
 # 4.2 - Double-check before clicking in 50-50 situations   - FIRST SOLVE, but rare
 # 4.3 - fixed random bug that ruined a lot (picking not the best known tile)  - more solves
-
-# 4.4 - corner priority
+# 4.4 - corner priority - more solves
 #%%
-DataRaw = [i for i in simulate_data(50000,0.33)]
+DataRaw = [i for i in simulate_data(500000,0.33)]
 DataAuged = [j for i in DataRaw for j in all_iso_variants(i)]
 X = np.array([i[0] for i in DataAuged])
 y = [i[1]-1 for i in DataAuged]
@@ -223,127 +232,151 @@ model.fit(X, y_one_hot, epochs=10, batch_size=50, validation_split=0.2)
 # print([i for i in all_iso_variants(DataRaw[0])])
 #%%
 #inputs
-map_width = 7
+map_width = 10
 mine_freq = 0.17
 
-#internal variables
-opened = []
-knowledge = []
-flags = []
-flags_chances = []
-temporal_uncertainty = []
-iteration = 0
-map = generate_map(map_width,mine_freq)
-mines_amount = sum([1 if i == 9 else 0 for i in map.flatten().tolist()])
-print_map(map, opened)
-
-pick = (np.random.randint(1,map_width-2),np.random.randint(1,map_width-2))
-while map[pick] == 9:
-    pick = (np.random.randint(1,map_width-2),np.random.randint(1,map_width-2))
-print(pick)
-recursive_open(pick)
-empty_recursive_cache()
-print_map(map, opened)
-
-
-while len(opened) < map_width**2:
-    dead_end = False
+too_fast = True
+while too_fast:
+    too_fast = True
+    opened = []
+    knowledge = []
+    flags = []
     flags_chances = []
-    if len(all_available()) <= len(temporal_uncertainty):
-        temporal_uncertainty = []
-    print("-"*map_width*2)
+    temporal_uncertainty = []
+    iteration = 0
+    map = generate_map(map_width,mine_freq)
+    mines_amount = sum([1 if i == 9 else 0 for i in map.flatten().tolist()])
     print_map(map, opened)
-    iteration += 1
 
-    # for tile in opened:
-    #     if tile not in flags and map[tile] != 0:
-    #         local_unkws = 0
-    #         local_mines = 0
-    #         ready_to_open = []
-    #         for adj in get_adjacent(tile):
-    #             if adj not in opened  and adj != "OoB":
-    #                 local_unkws += 1
-    #                 ready_to_open.append(adj)
-    #             if adj in flags:
-    #                 local_mines += 1
-    #                 local_unkws += 1
-    #         if local_unkws == map[tile] and local_mines != local_unkws:
-    #             for i in ready_to_open:
-    #                 flags.append(i)
-    #                 opened.append(i)
-
-    if not_flags() > 3:
-        knowledges = get_most_known()
-        pick = tuple([int(i) for i in knowledges[np.array([int(i) for i in knowledges[:, 1]]).argsort()[-1], 0].split(",")])
-        # print(knowledges)
-        # print([int(i) for i in knowledges[:, 1]])
-        # print(np.array([int(i) for i in knowledges[:, 1]]).argsort())
-        ladder_count = 0
-        while pick in temporal_uncertainty:
-            ladder_count += 1
-            if ladder_count > len(knowledges):
-                print("i think this is a dead end")
-                dead_end = True
-                break
-            else:
-                pick = tuple([int(i) for i in knowledges[knowledges[:, 1].argsort()][-1-ladder_count, 0].split(",")])
-    else:
-        pick = list(all_available())[np.random.randint(0,len(all_available()))]
-        while pick in temporal_uncertainty:
-            pick = list(all_available())[np.random.randint(0,len(all_available()))]
-    if dead_end:
-        pick = (np.random.randint(0,map_width-1),np.random.randint(0,map_width-1))
-        while pick in temporal_uncertainty or pick in opened:
-            pick = (np.random.randint(0,map_width-1),np.random.randint(0,map_width-1))
+    pick = (np.random.randint(1,map_width-2),np.random.randint(1,map_width-2))
+    while map[pick] == 9:
+        pick = (np.random.randint(1,map_width-2),np.random.randint(1,map_width-2))
     print(pick)
-    predictions = model.predict(np.array([get_slice(pick)]))
-    print(predictions)
-    predicted_classes = np.argmax(predictions, axis=1) + 1
-    decision = predicted_classes[0]
-    print(decision)
+    recursive_open(pick)
+    empty_recursive_cache()
+    print_map(map, opened)
 
-    if decision == 1 or not_flags() < 4:
-        # blind_predictions = model.predict(np.array([get_slice(pick, True)]))
-        # blind_classes = np.argmax(blind_predictions, axis=1) + 1
-        # if blind_classes[0] != decision and blind_predictions[0][1]-predictions[0][1] > 0.3:
-        #     print("Vague place")
-        #     temporal_uncertainty.append(pick)
-        #     for tile in get_adjacent(pick):
-        #         if tile in flags:
-        #             flags.remove(tile)
-        #             opened.remove(tile)
-        #             temporal_uncertainty.append(tile)
-        if pick not in temporal_uncertainty:
-            if map[pick] == 9:
-                print(cr.Fore.RED + "<< LOSS >>" + cr.Fore.RESET)
-                break
-            else:
-                print("Successful click")
-                recursive_open(pick)
-                empty_recursive_cache()
-    elif decision == 2 and pick not in temporal_uncertainty:
-        # if len(flags) <= mines_amount:
-        print("< Flag >")
-        flags.append(pick)
-        opened.append(pick)
-        for tile in [i for i in get_adjacent(pick) if i not in flags and i in opened]:
-            local_mines = []
-            for i in get_adjacent(tile):
-                if i in flags:
-                    local_mines.append(i)
-            if len(local_mines) > map[tile]:
-                print("Local overflow!")
-                for i in range(len(local_mines) - int(map[tile])):
-                    mine_chances = []
-                    for m in local_mines:
-                        print(m)
-                        predictions = model.predict(np.array([get_slice(m, True)]))
-                        print(predictions)
-                        predicted_classes = np.argmax(predictions, axis=1) + 1
-                        mine_chances.append(predictions[0][1])
-                    low = local_mines[mine_chances.index(min(mine_chances))]
-                    print("Removed:")
-                    print(low)
-                    flags.remove(low)
-                    opened.remove(low)
-                    temporal_uncertainty.append(low)
+
+    while len(opened) < map_width**2:
+        dead_end = False
+        flags_chances = []
+        if len(all_available()) <= len(temporal_uncertainty):
+            temporal_uncertainty = []
+        print("-"*map_width*2)
+        print_map(map, opened)
+        iteration += 1
+
+        if len(opened) > 8:
+            too_fast = False
+        # for tile in opened:
+        #     if tile not in flags and map[tile] != 0:
+        #         local_unkws = 0
+        #         local_mines = 0
+        #         ready_to_open = []
+        #         for adj in get_adjacent(tile):
+        #             if adj not in opened  and adj != "OoB":
+        #                 local_unkws += 1
+        #                 ready_to_open.append(adj)
+        #             if adj in flags:
+        #                 local_mines += 1
+        #                 local_unkws += 1
+        #         if local_unkws == map[tile] and local_mines != local_unkws:
+        #             for i in ready_to_open:
+        #                 flags.append(i)
+        #                 opened.append(i)
+
+        if not_flags() > 3:
+            knowledges = get_most_known()
+            pick = tuple([int(i) for i in knowledges[np.array([int(i) for i in knowledges[:, 1]]).argsort()[-1], 0].split(",")])
+            # print(knowledges)
+            # print([int(i) for i in knowledges[:, 1]])
+            # print(np.array([int(i) for i in knowledges[:, 1]]).argsort())
+            ladder_count = 0
+            while pick in temporal_uncertainty:
+                ladder_count += 1
+                if ladder_count > len(knowledges):
+                    print("i think this is a dead end")
+                    dead_end = True
+                    break
+                else:
+                    pick = tuple([int(i) for i in knowledges[knowledges[:, 1].argsort()][-1-ladder_count, 0].split(",")])
+        else:
+            pick = list(all_available())[np.random.randint(0,len(all_available()))]
+            while pick in temporal_uncertainty:
+                pick = list(all_available())[np.random.randint(0,len(all_available()))]
+        if dead_end:
+            pick = (np.random.randint(0,map_width-1),np.random.randint(0,map_width-1))
+            while pick in temporal_uncertainty or pick in opened:
+                pick = (np.random.randint(0,map_width-1),np.random.randint(0,map_width-1))
+        print(pick)
+        predictions = model.predict(np.array([get_slice(pick)]))
+        print(predictions)
+        predicted_classes = np.argmax(predictions, axis=1) + 1
+        decision = predicted_classes[0]
+        print(decision)
+
+        if decision == 1 or not_flags() < 4:
+            # blind_predictions = model.predict(np.array([get_slice(pick, True)]))
+            # blind_classes = np.argmax(blind_predictions, axis=1) + 1
+            # if blind_classes[0] != decision and blind_predictions[0][1]-predictions[0][1] > 0.3:
+            #     print("Vague place")
+            #     temporal_uncertainty.append(pick)
+            #     for tile in get_adjacent(pick):
+            #         if tile in flags:
+            #             flags.remove(tile)
+            #             opened.remove(tile)
+            #             temporal_uncertainty.append(tile)
+            if pick not in temporal_uncertainty:
+                if map[pick] == 9:
+                    print(cr.Fore.RED + "<< LOSS >>" + cr.Fore.RESET)
+                    break
+                else:
+                    print("Successful click")
+                    recursive_open(pick)
+                    empty_recursive_cache()
+                    local_mines = []
+                    for i in get_adjacent(pick):
+                        if i in flags:
+                            local_mines.append(i)
+                    if len(local_mines) > map[pick]:
+                        print("Local overflow!")
+                        for i in range(len(local_mines) - int(map[pick])):
+                            mine_chances = []
+                            for m in local_mines:
+                                print(m)
+                                predictions = model.predict(np.array([get_slice(m, True)]))
+                                print(predictions)
+                                predicted_classes = np.argmax(predictions, axis=1) + 1
+                                mine_chances.append(predictions[0][1])
+                            low = local_mines[mine_chances.index(min(mine_chances))]
+                            print("Removed:")
+                            print(low)
+                            flags.remove(low)
+                            opened.remove(low)
+                            temporal_uncertainty.append(low)
+        elif decision == 2 and pick not in temporal_uncertainty:
+            # if len(flags) <= mines_amount:
+            print("< Flag >")
+            flags.append(pick)
+            opened.append(pick)
+            for tile in [i for i in get_adjacent(pick) if i not in flags and i in opened]:
+                local_mines = []
+                for i in get_adjacent(tile):
+                    if i in flags:
+                        local_mines.append(i)
+                if len(local_mines) > map[tile]:
+                    print("Local overflow!")
+                    for i in range(len(local_mines) - int(map[tile])):
+                        mine_chances = []
+                        for m in local_mines:
+                            print(m)
+                            predictions = model.predict(np.array([get_slice(m, True)]))
+                            print(predictions)
+                            predicted_classes = np.argmax(predictions, axis=1) + 1
+                            mine_chances.append(predictions[0][1])
+                        low = local_mines[mine_chances.index(min(mine_chances))]
+                        print("Removed:")
+                        print(low)
+                        flags.remove(low)
+                        opened.remove(low)
+                        temporal_uncertainty.append(low)
